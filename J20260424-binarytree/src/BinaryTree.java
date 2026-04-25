c128568d0e4e7ab863bb0e74b820ed6a6aad474b
old-jiang <834638058@qq.com>
2026-04-25 21:36:26 +0800

    二叉树

--- J20260424-binarytree/src/BinaryTree.java
+++ J20260424-binarytree/src/BinaryTree.java
@@ -45,12 +45,105 @@ public class BinaryTree {
 
     // 中序遍历
     public void inOrder(TreeNode root) {
-
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
     }
 
     // 后序遍历
     public void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
 
+    public static int countSize = 0;
+    // 获取树中节点的个数  遍历思路 O(N)
+    public void size(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        countSize++;
+        size(root.left);
+        size(root.right);
+    }
+
+    //子问题思路 整棵树的节点个数 = 左子树的节点个数 + 右子树的节点个数 + 1
+    public int nodeSize(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return nodeSize(root.left) + nodeSize(root.right) + 1;
+    }
+
+    public static int leafCount = 0;
+    // 获取叶子节点的个数
+    public void getLeafNodeCount(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leafCount++;
+        }
+        getLeafNodeCount(root.left);
+        getLeafNodeCount(root.right);
+    }
+
+    public int getLeafNodeCount2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return getLeafNodeCount2(root.left) + getLeafNodeCount2(root.right);
+    }
+
+    // 获取第K层节点的个数
+    public int getKLevelNodeCount(TreeNode root,int k) {
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return getKLevelNodeCount(root.left, k - 1) + getKLevelNodeCount(root.right,k - 1);
+    }
+
+    // 获取二叉树的高度
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftTreeHeight = getHeight(root.left);
+        int rightTreeHeight = getHeight(root.right);
+        //return leftTreeHeight > rightTreeHeight ? leftTreeHeight + 1 : rightTreeHeight + 1;
+        return Math.max(leftTreeHeight,rightTreeHeight) + 1;
+    }
+
+    // 检测值为value的元素是否存在
+    public TreeNode find(TreeNode root, char val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        TreeNode ret = find(root.left,val);
+        if (ret != null) {
+            return ret;
+        }
+        TreeNode ret2 = find(root.right,val);
+        if (ret2 != null) {
+            return ret2;
+        }
+        return null;
     }
 
 }
--- J20260424-binarytree/src/Test.java
+++ J20260424-binarytree/src/Test.java
@@ -1,11 +1,83 @@
 public class Test {
 
+    //两棵树是否相同
+    public boolean isSameTree(BinaryTree.TreeNode p, BinaryTree.TreeNode q) {
+        if((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        }
+        //两个都为空 || 两个都不为空
+        if(p == null && q == null) {
+            return true;
+        }
+        //两个都不为空
+        if(p.val != q.val) {
+            return false;
+        }
+        //两个都不为空 && 值一样
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+
+    //另一棵树的子树
+    public boolean isSubtree(BinaryTree.TreeNode root, BinaryTree.TreeNode subRoot) {
+        if(root == null) {
+            return false;
+        }
+        if(isSameTree(root,subRoot)) {
+            return true;
+        }
+        if(isSubtree(root.left,subRoot)) {
+            return true;
+        }
+        if(isSubtree(root.right,subRoot)) {
+            return true;
+        }
+        return false;
+    }
+
+    public BinaryTree.TreeNode invertTree(BinaryTree.TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        BinaryTree.TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         BinaryTree.TreeNode root = binaryTree.createTree();
         System.out.print("前序遍历：");
         binaryTree.preOrder(root);
         System.out.println();
+        System.out.print("中序遍历：");
+        binaryTree.inOrder(root);
+        System.out.println();
+        System.out.print("后序遍历：");
+        binaryTree.postOrder(root);
+        System.out.println();
+        System.out.print("节点个数：");
+        binaryTree.size(root);
+        System.out.println(BinaryTree.countSize);
+        System.out.print("节点个数：");
+        System.out.println(binaryTree.nodeSize(root));
+        System.out.print("叶子节点的个数：");
+        binaryTree.getLeafNodeCount(root);
+        System.out.println(BinaryTree.leafCount);
+        System.out.print("叶子节点的个数：");
+        System.out.println(binaryTree.getLeafNodeCount2(root));
+        System.out.print("第三层的节点个数：");
+        System.out.println(binaryTree.getKLevelNodeCount(root, 3));
+        System.out.print("树的高度：");
+        System.out.println(binaryTree.getHeight(root));
+        System.out.print("是否存在字符'F'：");
+        BinaryTree.TreeNode node = binaryTree.find(root,'F');
+        System.out.println(node.val);
     }
 
 }
